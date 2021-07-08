034eaba284770ee8ee458450b0ea30593b29e8a2
Denis D. Alvarez Useda <dalvarezu@muniate.gob.pe>
2021-07-08 07:23:19 -0500

    se agrego enlace

--- index.php
+++ index.php
@@ -14,5 +14,6 @@
 ?>
     <p style="color:blue;">Mi nombre es <span><?php echo $nombre;?></span> y vivo en la <span><?php echo $direccion; ?></span> </p>
     <p style="color:royalblue;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ut. Illo a earum adipisci iure, totam recusandae rerum nemo architecto, corporis neque officiis quod accusantium excepturi non quasi voluptatum rem.</p>
+    <p>Ingresar al siguiente enlace: <a href="#">www.youtube.com</a></p>
 </body>
 </html>
