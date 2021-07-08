abe7866cca1feb558702fb12204ea9795d79ef50
Denis D. Alvarez Useda <dalvarezu@muniate.gob.pe>
2021-07-08 08:50:20 -0500

    se quito enlace y cambio de color al titulo

--- index.php
+++ index.php
@@ -7,13 +7,12 @@
     <title>Prueba AWS</title>
 </head>
 <body>
-    <center><h1>Prueba AWS</h1></center>
+    <center><h1 style="color:blue;">Prueba AWS</h1></center>
 <?php 
     $nombre = "denis";
     $direccion = "calle guillen";
 ?>
     <p style="color:blue;">Mi nombre es <span><?php echo $nombre;?></span> y vivo en la <span><?php echo $direccion; ?></span> </p>
     <p style="color:royalblue;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ut. Illo a earum adipisci iure, totam recusandae rerum nemo architecto, corporis neque officiis quod accusantium excepturi non quasi voluptatum rem.</p>
-    <p>Ingresar al siguiente enlace: <a href="#">www.youtube.com</a></p>
 </body>
 </html>
