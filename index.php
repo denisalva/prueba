2049d9977a9386d4b09cfcc7a950ac5f9ba8c78b
daysfade <denis.alvau@gmail.com>
2021-07-07 22:10:40 -0500

    estilo html

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba AWS</title>
+</head>
+<body>
+    <h1>Prueba AWS</h1>
 <?php 
     $nombre = "denis";
     $direccion = "calle guillen";
-    echo '<p style="color:red;">'.$nombre.' '.$direccion.'</p>';
 ?>
+    <p>Mi nombre es <span><?php echo $nombre;?></span> y vivo en la <span><?php echo $direccion; ?></span> </p>
+</body>
+</html>
