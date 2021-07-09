1f0a204df7e198f57457e19e8a102c20bcfd5780
Denis D. Alvarez Useda <dalvarezu@muniate.gob.pe>
2021-07-09 07:57:01 -0500

    se cambio de color al titulo y se elimino texto

--- index.php
+++ index.php
@@ -7,12 +7,11 @@
     <title>Prueba AWS</title>
 </head>
 <body>
-    <center><h1 style="color:blue;">Prueba AWS</h1></center>
+    <center><h1>Prueba AWS</h1></center>
 <?php 
     $nombre = "denis";
     $direccion = "calle guillen";
 ?>
     <p style="color:blue;">Mi nombre es <span><?php echo $nombre;?></span> y vivo en la <span><?php echo $direccion; ?></span> </p>
-    <p style="color:royalblue;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, ut. Illo a earum adipisci iure, totam recusandae rerum nemo architecto, corporis neque officiis quod accusantium excepturi non quasi voluptatum rem.</p>
 </body>
 </html>
